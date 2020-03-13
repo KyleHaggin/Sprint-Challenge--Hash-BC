7836b605afa3f179565c6f42672a1e8860640ea6
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-03-13 09:27:44 -0700

    ex1.py saved work

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,11 +7,24 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
+    # Create has table
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
+    # Index and insert the hash table
+    index = 0
+    for weight in weights:
+        hash_table_insert(ht, weight, index)
+        index += 1
+    for weight in weights:
+        # weight to find
+        target_weight = limit - weight
+        target_index = hash_table_retrieve(ht, target_weight)
+        if target_index is not None:
+            hash_table_remove(ht, target_weight)
+            weight_index = hash_table_retrieve(ht, weight)
+            if weight_index is None:
+                weight_index = 0
+            print([target_index, weight_index])
+            return [target_index, weight_index]
 
     return None
 
